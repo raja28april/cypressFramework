84c2c335f2d81f41f709717d4d131b111307f2a6
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-11-03 08:08:43 +0100

    Raja: handling iframes and also typing in it

---
+++ cypress/e2e/testcases/16.iframes.cy.js
@@ -0,0 +1,29 @@
+describe('Iframes handlind', () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('demoqa')}/nestedframes`);
+    });
+    it('sample and nested iframes', () => {
+        cy.get('#frame1').then($iframe => {
+            const $body = $iframe.contents().find("body");
+            cy.wrap($body).should('have.text', 'Parent frame');
+            cy.wrap($body).find('iframe').then($childFrame => {
+                const $childBody = $childFrame.contents().find("body");
+                // cy.wrap($childBody).should('have.text', 'Child Iframe');
+                cy.wrap($childBody).find('p').should('have.text', 'Child Iframe');//another approach
+            })
+        })
+    });
+});
+
+describe.only('Typing in an iframe - the internet app', () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('theinternet')}/iframe`);
+    });
+    it('Iframe demo', () => {
+        cy.get('#mce_0_ifr').then(iframe => {
+            const $body = iframe.contents().find('body');
+            cy.wrap($body).find('p').type(`{selectAll}Hello world!`);
+            cy.wrap($body).find('p').should('have.text', 'Hello world!');
+        })
+    });
+});
